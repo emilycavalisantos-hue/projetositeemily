438ea3c5b170c48b04ee74c1f9a010a2f5438029
emilycavalisantos-hue <emily.cavali.santos@escola.pr.gov.br>
2026-09-01 21:43:51 -0300

    ;;;;

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const botoes = document.querySelectorAll("button");
-botoes.forEach (function(botao) {
-  botao.addE
+const botoes = document.querySelectorA11("button");
 
-});
-function botaoClicado() {
-  console.log("fui clicado");
-  let texto = botao.querySelector("span");
-  texto.textContent++;
+
+botoes.forEach(function (botao) {
+   botao.addEventListener("click", botaoClicado);
+   function botaoClicado () {
+       console.log ("fui clicado");
+       let texto = botao.querySelector("span");
+       texto.textContent++;
 }
-
-
+});
